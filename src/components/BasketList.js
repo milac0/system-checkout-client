6aea6113e3a82c3bd9489b0e79f9ca02fcd4c1b1
unknown <milac0@hotmail.com>
2020-01-17 11:43:06 +0100

    add list in basket

---
+++ src/components/BasketList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
+import Item from "./Item";
+import "./basketList.scss";
+
+const BasketList = () => {
+  const { state } = useContext(DataContext);
+  return (
+    <div className="basket_list">
+      {state.basket.length ? (
+        state.basket.map(bask => <Item item={bask} />)
+      ) : (
+        <p className="basket_msg">Empty basket</p>
+      )}
+    </div>
+  );
+};
+
+export default BasketList;
--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,7 +1,12 @@
 import React from "react";
+import BasketList from "./../components/BasketList";
 
 const Basket = () => {
-  return <div>hello basket</div>;
+  return (
+    <div>
+      <BasketList />
+    </div>
+  );
 };
 
 export default Basket;
