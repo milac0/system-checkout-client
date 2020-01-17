7afe594709b880de8cb0086d35e9fc3e08333eba
unknown <milac0@hotmail.com>
2020-01-17 10:56:58 +0100

    add styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { DataContext } from "../context/DataContext";
 import { ADD_ITEM, REMOVE_ITEM } from "../context/types";
+import plusImg from "../assets/images/plus.png";
+import minusImg from "../assets/images/minus.png";
+import "./item.scss";
 
 const Item = ({ item }) => {
   const { state, dispatch } = useContext(DataContext);
@@ -28,12 +31,20 @@ const Item = ({ item }) => {
 
   const { name, price, _id } = item;
   return (
-    <div>
-      <p>{name}</p>
-      <p>{price}</p>
-      <button onClick={() => handleAdd(item)}>+</button>
-      <p>{getCount(_id) || 0}</p>
-      <button onClick={() => handleRemove(_id)}>i</button>
+    <div className="item">
+      <div className="container info">
+        <h1>{name}</h1>
+        <p>{price}</p>
+      </div>
+      <div className="container buttons">
+        <button onClick={() => handleRemove(_id)}>
+          <img src={minusImg} />
+        </button>
+        <p>{getCount(_id) || 0}</p>
+        <button onClick={() => handleAdd(item)}>
+          <img src={plusImg} />
+        </button>
+      </div>
     </div>
   );
 };
